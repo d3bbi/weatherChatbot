bd1105d5005dcb4fe2e3c867eb18677b179eabd9
David <david.oreilly@student.griffith.ie>
2021-03-30 16:49:45 +0000

    Update chatbot.php with JavaScript

--- chatbot.php
+++ chatbot.php
@@ -29,6 +29,41 @@
             </div>
         </div>
     </div>
+  <script>
+        $(document).ready(function(){
+            $("#send-btn").on("click", function(){
+				$question = $("#question").html();
+                $value = $("#data").val();
+				$cities = $('#cities').val();
+                $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>'+ $value +'</p></div></div>';
+                $(".form").append($msg);
+                $("#data").val('');
+				
+				
+                // start ajax code
+                $.ajax({
+                    url: 'message.php',
+                    type: 'POST',
+					data: {question: $question, text: $value, cities: $cities},
+                    //data: 'text='+$value,
+                    success: function(result){
+						
+						var jsArray = JSON.parse(result);
+						console.log(jsArray);
+						$replay = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header">'+ jsArray[1] +'</div></div>';
+                        //$replay = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
+                        $(".form").append($replay);
+						$(".results").append($replay);
+                        // when chat goes down the scroll bar automatically comes to the bottom
+                        $(".form").scrollTop($(".form")[0].scrollHeight);
+						$("#question").html(jsArray[0]);
+						$("#cities").val(jsArray[3]);
+						console.log($cities);
+                    }
+                });
+            });
+        });
+    </script>
 
 </body>
 </html>
