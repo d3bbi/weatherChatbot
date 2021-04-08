8017bd83027ab3aa73514ccd2b0445bb17b1141e
Cian <cian.scarborough@student.griffith.ie>
2021-04-08 12:16:58 +0100

    Renaming files to make more sense

--- functionsTest.php
+++ functionsTest.php
@@ -3,7 +3,7 @@ require './vendor/autoload.php';
 
 
 
-class processDateTest extends PHPUnit\Framework\TestCase {
+class functionsTest extends PHPUnit\Framework\TestCase {
 /**
  * processDate() test case.
  */
@@ -11,7 +11,7 @@ class processDateTest extends PHPUnit\Framework\TestCase {
 	//Testing process date function returns correct result when date passed.
     public function testProcessDatePositiveReturn(): void
     {
-		require "processDate.php";
+		require "functions.php";
 		
 		$expected = array("country","Great, now which country are you planning to visit?","20-06-2021");
 		
@@ -20,7 +20,7 @@ class processDateTest extends PHPUnit\Framework\TestCase {
 	//Testing process date function returns correct result when non date passed.
     public function testProcessDateNegativeReturn(): void
     {
-		require "processDate.php";
+		require "functions.php";
 		$expected = array("date","I'm sorry I didn't get that date. Can you try again?","banana");
 		
         $this->assertEquals($expected,$processDate("banana"));
