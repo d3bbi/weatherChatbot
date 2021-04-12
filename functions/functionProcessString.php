13a729cb80f4914b29e1bb3918e7f3a8c24bf39e
David O'Reilly <david.oreilly@student.griffith.ie>
2021-04-12 13:52:49 +0100

    added else if for checking for country comment with expected output

--- functions/functionProcessString.php
+++ functions/functionProcessString.php
@@ -6,4 +6,7 @@ $processString = function ($getAnswerExpected, $getMsg, $getCities){
         if ($getAnswerExpected == "date"){
             // Check if check $getMsg contains a date and return just the date part of the string 
         }
+        else if($getAnswerExpected === "country"){
+            //Check if a country value exists in the $getMsg string return just the country
+        }
 };
