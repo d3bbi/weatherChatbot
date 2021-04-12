937b5ca5c74acae1588704a8223481e7af955c3c
David O'Reilly <david.oreilly@student.griffith.ie>
2021-04-12 13:50:35 +0100

    Added first if logic with comment for expected output

--- functions/functionProcessString.php
+++ functions/functionProcessString.php
@@ -3,5 +3,7 @@
 //function to check if value is a date
 
 $processString = function ($getAnswerExpected, $getMsg, $getCities){
-
+        if ($getAnswerExpected == "date"){
+            // Check if check $getMsg contains a date and return just the date part of the string 
+        }
 };
