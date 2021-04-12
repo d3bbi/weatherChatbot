b2cc7a7a2f61af416495f43b8ab0831e16936359
David O'Reilly <david.oreilly@student.griffith.ie>
2021-04-12 13:59:50 +0100

    else if for getting city from array with comment

--- functions/functionProcessString.php
+++ functions/functionProcessString.php
@@ -9,4 +9,8 @@ $processString = function ($getAnswerExpected, $getMsg, $getCities){
         else if($getAnswerExpected === "country"){
             //Check if a country value exists in the $getMsg string return just the country
         }
+        else if($getAnswerExpected === "city"){
+            // explode the $getCities string into an array and check does any word in the $getMsg string match a city a city in array.
+            // If match found, return just the city
+        }
 };
