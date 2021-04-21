e2841c670ff588712df4781b39f37cd024d2b06b
Deborah-R <deborah_rimei@hotmail.com>
2021-04-21 15:25:01 +0100

    convert kelvin to celsius

--- functions/functionProcessCity.php
+++ functions/functionProcessCity.php
@@ -14,7 +14,7 @@ $found = false;
         $ch = curl_init();
 
         // set url
-        curl_setopt($ch, CURLOPT_URL, "http://api.openweathermap.org/data/2.5/weather?q=Ireland&appid=a6b398ac3fdaeeb64f5fa4d343043948");
+        curl_setopt($ch, CURLOPT_URL, "http://api.openweathermap.org/data/2.5/weather?q=".$getMesg."&appid=a6b398ac3fdaeeb64f5fa4d343043948");
 
         //return the transfer as a string
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -22,8 +22,11 @@ $found = false;
         //$output contains the output string
         $output = curl_exec($ch);
 		$js_code = json_decode($output);
-		//$js_code->weather[0]->description . '<br>' . $js_code->main->temp;
-		$array = array("weather","The weather in ".$getMesg." is ".$js_code->weather[0]->description . " with a temperature of: " . $js_code->main->temp,$getMesg,$getCities,$js_code->weather[0]->description,$js_code->main->temp);
+		// $js_code->weather[0]->description . '<br>' . $js_code->main->temp;
+		
+		//calculate temperature - Celsius = Kelvin-273.15
+		$temp = "<br>".round($js_code->main->temp-273.15,1)." C";
+		$array = array("weather","The weather in ".$getMesg." is ".$js_code->weather[0]->description . " with a temperature of: " . $temp,$getMesg,$getCities,$js_code->weather[0]->description,$js_code->main->temp);
 		return $array;
         // close curl resource to free up system resources
         curl_close($ch);
